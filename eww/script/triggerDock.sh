cd1c42153b54f09691b4375539c2b284799077e2
EvanKoe <evan.koehler@epitech.eu>
2023-10-16 00:58:37 +0200

    [ADD] at glance widget

--- eww/script/triggerDock.sh
+++ eww/script/triggerDock.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-ISSHOWN=`eww get isDockShown`
-echo " old : $ISSHOWN"
-
-if [ "$ISSHOWN" = "false" ]; then
-  eww update isDockShown="true"
-else
-  eww update isDockShown="false"
+if [ "$1" -z ]; then
+  echo "USAGE: ./triggerDock.sh VARIABLE [--toggle] NEW_VALUE"
+  exit 1
 fi
 
-echo "new: $(eww get isDockShown)"
+TOGGLE=false
+
+while [ $# -ne 0 ]
+do
+  ARG="$1"
+  if [ "$ARG" = "--toggle" ]; then
+    TOGGLE=true
+  fi
+done
+
+ISSHOWN=`eww get isDockShown`
+echo "old : $ISSHOWN"
+
+if [ "$ISSHOWN" = "false" ]; then
+  RET=`eww update isDockShown="true"`
+else
+  RET=`eww update isDockShown="false"`
+fi
+
+echo "new : $(eww get isDockShown)"
